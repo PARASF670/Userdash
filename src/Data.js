4496af3fd6a2f41f9ced76524ea05f903a26fb88
PARASF670 <42441544+PARASF670@users.noreply.github.com>
2021-10-09 11:29:16 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,85 @@
+var cards = [
+    {
+        img: "https://via.placeholder.com/450x300",
+        title: "Fancy Product",
+        Price: "$40.00 - $80.00",
+        star: 3,
+        btitle: "Add to cart",
+        sale: false,
+        id:1
+    },
+    {
+        img: "https://via.placeholder.com/450x300",
+        title: "Special Item",
+        Price: "$20.00 - $18.00",
+        star: 5,
+        btitle: "Add to cart",
+        sale: true,
+        id:2
+    },
+    {
+        img: "https://via.placeholder.com/450x300",
+        title: "Sale item",
+        Price: "$50.00 - $25.00",
+        star: 2,
+        btitle: "Add to cart",
+        sale: true,
+        id:3
+    },
+    {
+        img: "https://via.placeholder.com/450x300",
+        title: "Popular Item",
+        Price: "$40.00 ",
+        star: 5,
+        btitle: "Add to cart",
+        sale: false,
+        id:4
+    },
+    {
+        img: "https://via.placeholder.com/450x300",
+        title: "Sale Item",
+        Price: "$50.00 - $25.00 ",
+        star: 4,
+        btitle: "Add to cart",
+        sale: true,
+        id:5
+    },
+    {
+        img: "https://via.placeholder.com/450x300",
+        title: "Fancy Product",
+        Price: "$120.00 - $280.00",
+        star: 3,
+        btitle: "Add to cart",
+        sale: false,
+        id:6
+    },
+    {
+        img: "https://via.placeholder.com/450x300",
+        title: "Special Item",
+        Price: "$20.00 - $18.00 ",
+        star: 5,
+        btitle: "Add to cart",
+        sale: true,
+        id:7
+    }, {
+        img: "https://via.placeholder.com/450x300",
+        title: "Popular Item",
+        Price: "$40.00",
+        star: 5,
+        btitle: "Add to cart",
+        sale: false,
+        id:8
+    },
+    {
+        img: "https://via.placeholder.com/450x300",
+        title: "Fancy Product",
+        Price: "$120.00 - $280.00",
+        star: 3,
+        btitle: "Add to cart",
+        sale: false,
+        id:9
+    },
+    
+]
+
+export default cards;
